a99d9b41cc72ee69a077914471dad3d725986fa8
kenton116 <kento_nakamura@outlook.com>
2023-03-26 19:19:59 +0900

    トップページ作成

--- teamist/lib/main.dart
+++ teamist/lib/main.dart
@@ -1,31 +1,107 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'top.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  final _router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        pageBuilder: (context, state) => MaterialPage(
+          child: TopPage(),
+        ),
+      ),
+      GoRoute(
+        path: '/sign-in',
+        pageBuilder: (context, state) => MaterialPage(
+          child: BasicPage(title: 'ログイン',)
+        ),
+      ),
+      GoRoute(
+        path: '/sign-up',
+        pageBuilder: (context, state) => MaterialPage(
+          child: BasicPage(title: 'アカウント作成',)
+        ),
+      ),
+    ],
+    errorPageBuilder: (_, state) => MaterialPage(
+      key: state.pageKey,
+      child: ErrorPage(),
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'teamist',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MaterialApp.router(
+      routeInformationProvider: _router.routeInformationProvider,
+      routeInformationParser: _router.routeInformationParser,
+      routerDelegate: _router.routerDelegate,
+    );
+  }
+}
+
+class TopPage extends StatelessWidget {
+  const TopPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFc3dbde),
+      body: ListView(
+        children: [
+          ListTile(
+            onTap: () => context.go('/sign-in'),
+            title: Text('ログイン'),
+          ),
+          ListTile(
+            onTap: () => context.go('/sign-up'),
+            title: Text('アカウント作成'),
+          ),
+        ],
       ),
     );
   }
+}
 
-  final _router = GoRouter(initialLocation: '/', routes: [
-    GoRoute(
-      name: 'top',
-      path: '/',
-      pageBuilder: (context, state) => MaterialPage(
-        key: state.pageKey,
-        child: const TopPage(),
+class BasicPage extends StatelessWidget {
+  const BasicPage({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        leading: IconButton(
+          onPressed: () => context.go('/'),
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
       ),
-    ),
-  ]);
+      body: Center(
+        child: Text(title),
+      ),
+    );
+  }
+}
+
+class ErrorPage extends StatelessWidget {
+  const ErrorPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Error'),
+      ),
+    );
+  }
 }
